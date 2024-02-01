6ffb31762ca9b8b6500893c7a0f27a2c100840c1
Vijay <dhruvakumar.gopisetty@gmail.com>
2024-02-01 22:42:51 +0530

    v2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,14 @@ public class Main {
   B obj2=new B();
   String str2= obj2.meth();
 }
+class A{
+  public String meth(){
+    return "Invoking method from class A";
+  }
+}
+class B extends A{
+  @Override
+  public String meth(){
+    return "Method is overridden in Extendend class B";
+  }
+}
